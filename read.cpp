ad8c576b6f3203e3dd5a09a4a30b16c26abd9dcf
Alex MacNair <amacnair14@gmail.com>
2018-02-05 13:26:04 -0500

    Started reading file data

--- read.cpp
+++ read.cpp
@@ -9,7 +9,50 @@
 #include <sys/time.h>
 
 #include "constants.h"
+#include "filesystem.h"
+
+
+
+// First, check whether we're allowed to read the file, etc
+// if we aren't, then send an error to the client and abort
+FILE* getFile(int socket, struct sockaddr_in client, char* filename)
+{
+	socklen_t clientLen = sizeof(client);
+	FILE* f = NULL;
+	f = openFileRead(filename);
+    fileError error;
+	if( f <= 0 )
+	{
+		error = checkFileError(errno);
+		sendto(socket, error.header[error.headerNum], strlen(error.header[error.headerNum]+4)+4, 0, (struct sockaddr*)&client, clientLen);
+		exit(EXIT_FAILURE);
+	}
+	return f;
+}
+
 
 void readRequest(char* filename, char* mode, struct sockaddr_in client) {
+
+    int blocksSent = 0; // Track the number of blocks sent
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	socklen_t clientLen = sizeof(client);
+
+	FILE* f = getFile(sock, client, filename);
+
+    char buffer[DATABLOCK_SIZE];
+    if (f != NULL)
+    {
+        int read = 0;
+        while ((read = fread(buffer, 1, DATABLOCK_SIZE, f)) > 0)
+        {
+            for (int i=0; i < read; i++)
+            {
+
+            }
+        }
+    }
+
+
+
 
 }
